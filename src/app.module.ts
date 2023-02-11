91b40b887e84d6b641148c983a3c7ee6a1edf487
Erick Pham <erick.pham.dev@gmail.com>
2023-02-11 11:39:26 +0700

    add throttler, helmet

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,25 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { dataSourceOptions } from 'src/database/data-source';
 import { LoggerMiddleware } from 'src/middleware/logger.middleware';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('NestJS Starter')
